1088f76a86fc033eaddde36bf62247b236e059dc
ChernenkoDanya <86494434+ChernenkoDanya@users.noreply.github.com>
2022-12-14 19:51:33 +0300

    Add files via upload

---
+++ parse_settings.py
@@ -0,0 +1,10 @@
+
+# List of genres. Currently available genres: detectives, fiction, horrors, fantasy, romance, action
+# by default: ['detectives', 'fiction', 'horrors', 'fantasy', 'romance', 'action']
+GENRES = ['detectives', 'fiction', 'horrors', 'fantasy', 'romance', 'action']
+
+# Name of a file containing parsing results (books by default)
+OUTPUT_FILE_NAME = 'books'
+
+# Format of a file containing parsing results (json by default)
+OUTPUT_FILE_FORMAT = 'json'
